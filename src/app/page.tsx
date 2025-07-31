88676005f0d586476ff734f55e1f82d27ecc4413
MurtazaD1410 <dhariwalamurtaza22@gmail.com>
2025-07-31 20:25:26 +0200

    09: Projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@
 
 import { toast } from "sonner";
 import { useState } from "react";
-import { useMutation, useQuery } from "@tanstack/react-query";
-
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
@@ -11,25 +11,29 @@ import { Input } from "@/components/ui/input";
 const Page = () => {
   const [value, setValue] = useState("");
   const trpc = useTRPC();
-  const { data: messages } = useQuery(trpc.messages.getMany.queryOptions());
-  const createMessage = useMutation(
-    trpc.messages.create.mutationOptions({
-      onSuccess: () => {
-        toast.success("Message Created!");
+  const router = useRouter();
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onError: (error) => {
+        toast.error(error.message);
+      },
+      onSuccess(data) {
+        router.push(`/projects/${data.id}`);
       },
     })
   );
 
   return (
-    <div className="p-4 max-w-7xl mx-auto">
-      <Input value={value} onChange={(e) => setValue(e.target.value)} />
-      <Button
-        disabled={createMessage.isPending}
-        onClick={() => createMessage.mutate({ value: value })}
-      >
-        Invoke background job
-      </Button>
-      <pre>{JSON.stringify(messages, null, 2)}</pre>
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="max-w-7xl mx-auto flex items-center flex-col gap-y-4 justify-center w-full">
+        <Input value={value} onChange={(e) => setValue(e.target.value)} />
+        <Button
+          disabled={createProject.isPending}
+          onClick={() => createProject.mutate({ value: value })}
+        >
+          Submit
+        </Button>
+      </div>
     </div>
   );
 };
