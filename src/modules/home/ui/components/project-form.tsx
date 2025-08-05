5d466396c1d9968f0fa49fe622ceca0e5110fb26
MurtazaD1410 <dhariwalamurtaza22@gmail.com>
2025-08-05 07:23:18 +0200

    14: Home Page

---
+++ src/modules/home/ui/components/project-form.tsx
@@ -0,0 +1,147 @@
+"use client";
+
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import TextAreaAutosize from "react-textarea-autosize";
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { Form, FormField } from "@/components/ui/form";
+import z from "zod";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { PROJECT_TEMPLATES } from "../../constants";
+
+const formSchema = z.object({
+  value: z
+    .string()
+    .min(1, {
+      message: "Prompt is required",
+    })
+    .max(10000, {
+      message: "Prompt is too long",
+    }),
+});
+
+const ProjectForm = () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const trpc = useTRPC();
+  const router = useRouter();
+
+  const queryClient = useQueryClient();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "",
+    },
+  });
+
+  const createProject = useMutation(
+    trpc.projects.create.mutationOptions({
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(trpc.projects.getMany.queryOptions());
+        router.push(`/projects/${data.id}`);
+        // ! TODO: invalidate usage status
+      },
+      onError: (error) => {
+        // ! TODO : Redirect to specific page if error
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const isPending = createProject.isPending;
+  const isButtonDisabled = isPending || !form.formState.isValid;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    await createProject.mutateAsync({
+      value: values.value,
+    });
+  };
+
+  const onSelect = (value: string) => {
+    form.setValue("value", value, {
+      shouldValidate: true,
+      shouldTouch: true,
+      shouldDirty: true,
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <section className="space-y-6">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className={cn(
+            "relative border p-4 pt-1 rounded-xl bg-sidebar dark:bg-sidebar transition-all",
+            isFocused && "shadow-xs"
+          )}
+        >
+          <FormField
+            control={form.control}
+            name="value"
+            render={({ field }) => (
+              <TextAreaAutosize
+                {...field}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => setIsFocused(false)}
+                minRows={2}
+                maxRows={8}
+                disabled={isPending}
+                className="pt-4 border-none w-full outline-none resize-none bg-transparent"
+                placeholder="What would you like to build?"
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" && (e.ctrlKey || e.metaKey)) {
+                    e.preventDefault();
+                    form.handleSubmit(onSubmit)(e);
+                  }
+                }}
+              />
+            )}
+          />
+          <div className="flex gap-x-2 items-end justify-between pt-2">
+            <div className="text-[10px] text-muted-foreground font-mono">
+              <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+                <span className="">&#8984;</span>Enter
+              </kbd>
+              &nbsp;to submit
+            </div>
+            <Button
+              className={cn(
+                "size-8 rounded-full",
+                isButtonDisabled && "bg-muted-foreground border"
+              )}
+              disabled={isButtonDisabled}
+            >
+              {isPending ? (
+                <Loader2Icon className="size-4 animate-spin" />
+              ) : (
+                <ArrowUpIcon />
+              )}
+            </Button>
+          </div>
+        </form>
+        <div className="flex-wrap justify-center gap-2 hidden md:flex max-w-3xl">
+          {PROJECT_TEMPLATES.map((template) => (
+            <Button
+              key={template.title}
+              variant={"outline"}
+              size={"sm"}
+              className="bg-white dark:bg-sidebar"
+              onClick={() => onSelect(template.prompt)}
+            >
+              {template.emoji} {template.title}
+            </Button>
+          ))}
+        </div>
+      </section>
+    </Form>
+  );
+};
+
+export default ProjectForm;
